bdbf7fb9bdd6b3323e3b7f5388292175f7fa13df
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-10-08 02:08:25 +0200

    Heroku settings for debug deployment

--- django_cms/django_cms/settings/production.py
+++ django_cms/django_cms/settings/production.py
@@ -1,6 +1,18 @@
 from .base import *
 
+
 DEBUG = environ.get('DJANGO_DEBUG', False)
 TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = [
+    'https://devfestitaly.herokuapp.com',
+]
+
 SITE_ID = 2
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
