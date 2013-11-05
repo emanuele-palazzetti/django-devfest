84b9c479cad6d2d0ab1288111dbae5575b7a848b
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-11-05 01:53:24 +0100

    Added ALLOWED_HOSTS

--- django_cms/django_cms/settings/production.py
+++ django_cms/django_cms/settings/production.py
@@ -12,7 +12,8 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = [
-    'https://devfestitaly.herokuapp.com',
+    'devfestitaly.herokuapp.com',
+    'devfest.gdg.io',
 ]
 
 SITE_ID = 2
